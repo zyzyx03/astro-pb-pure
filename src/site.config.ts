0e9bda91b537ac1fdf1d95cdc6aef8e5b052b526
zyzyx03 <fauzuwan.nazri93@gmail.com>
2025-11-21 21:01:02 +0800

    feat(comments)remove Waline components enable blog commenting

--- src/site.config.ts
+++ src/site.config.ts
@@ -159,7 +159,7 @@ export const integ: IntegrationUserConfig = {
   },
   // Comment system
   waline: {
-    enable: true,
+    enable: false,
     // Server service link
     server: 'https://astro-theme-pure-waline.arthals.ink/',
     // Refer https://waline.js.org/en/guide/features/emoji.html
